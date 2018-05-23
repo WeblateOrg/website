ee1e5b893d2ca4feb8b8893f28231645830c6c7c
Michal Čihař <michal@cihar.com>
2018-05-23 17:55:12 +0200

    Add Unknown Horizons addon
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ wlhosted/addons.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2018 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""
+Example pre commit script
+"""
+
+from __future__ import unicode_literals
+
+from django.utils.translation import ugettext_lazy as _
+
+from weblate.addons.events import EVENT_PRE_COMMIT
+from weblate.addons.scripts import BaseScriptAddon
+
+
+class ExamplePreAddon(BaseScriptAddon):
+    # Event used to trigger the script
+    events = (EVENT_PRE_COMMIT,)
+    # Name of the addon, has to be unique
+    name = 'weblate.hosted.uh_scenario'
+    # Verbose name and long descrption
+    verbose = _('Generate Unknown Horizons scenario data')
+    description = _('Generates scenario data for Unknown Horizons.')
+
+    # Script to execute
+    script = '/home/nijel/bin/uh_scenario_yaml'
+    # File to add in commit (for pre commit event)
+    # does not have to be set
+    add_file = 'content/scenarios/*_{{ language_code }}.yaml'
+
+    @classmethod
+    def can_install(cls, component, user):
+        """Only useful for Unknown Horizons project"""
+        if not component.project.slug == 'uh':
+            return False
+        return super(DiscoveryAddon, cls).can_install(component, user)
