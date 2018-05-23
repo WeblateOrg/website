b491c6fe30bdf58f4dfd14229f972c5807967bd8
Michal Čihař <michal@cihar.com>
2018-05-23 17:49:16 +0200

    Initiap package structure
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2018 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import io
+import os
+import sys
+from setuptools import setup
+
+setup(
+    name='wlhosted',
+    version='0.1',
+    packages=[
+        'wlhosted',
+    ],
+    license='GPLv3+',
+    description=(
+        'Hosted Weblate Customization'
+    ),
+    url='https://weblate.org/',
+    download_url='https://weblate.org/download/',
+    bugtrack_url='https://github.com/WeblateOrg/hosted/issues',
+    author='Michal Čihař',
+    author_email='michal@cihar.com',
+    zip_safe=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: '
+        'GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Software Development :: Localization',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
