9d199871c3751782d799c0c0523f088c2ac25882
Michal Čihař <michal@cihar.com>
2018-10-23 16:07:08 +0200

    Add for for creating new payment
    It still needs proper UI
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ wlhosted/forms.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2018 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from __future__ import unicode_literals
+
+from django import forms
+
+from weblate.billing.models import Plan
+from wlhosted.models import Payment, Customer
+
+
+class ChooseBillingForm(forms.Form):
+    plan = forms.ChoiceField(choices=[])
+    period = forms.ChoiceField(choices=[('y', 'y'), ('m', 'm')])
+    extra_domain = forms.BooleanField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(ChooseBillingForm, self).__init__(*args, **kwargs)
+        self.fields['plan'].choices = [
+            (p, p) for p in Plan.objects.values_list('id', flat=True)
+        ]
+
+    def create_payment(self, user):
+        customer = Customer.objects.create(email=user.email)
+        plan = Plan.objects.get(pk=self.cleaned_data['plan'])
+        period = self.cleaned_data['period']
+        description = 'Weblate hosting ({}, {})'.format(
+            plan.name,
+            'Monthly' if period == 'm' else 'Yearly'
+        )
+        amount = plan.price if period == 'm' else plan.yearly_price
+        if self.cleaned_data['extra_domain']:
+            amount += 100
+            description += ' + Custom domain'
+        return Payment.objects.create(
+            amount=amount,
+            description=description,
+            recurring=self.cleaned_data['period'],
+            customer=customer,
+        )
---
+++ wlhosted/views.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2018 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.utils.decorators import method_decorator
+from django.utils.translation import get_language
+from django.views.generic.edit import FormView
+
+from weblate.billing.models import Plan
+
+from wlhosted.forms import ChooseBillingForm
+
+# List of supported languages on weblate.org
+SUPPORTED_LANGUAGES = frozenset((
+    'ar', 'az', 'be', 'be@latin', 'bg', 'br', 'ca', 'cs', 'da', 'de', 'en',
+    'el', 'en-gb', 'es', 'fi', 'fr', 'gl', 'he', 'hu', 'id', 'it', 'ja', 'ko',
+    'nb', 'nl', 'pl', 'pt', 'pt-br', 'ru', 'sk', 'sl', 'sq', 'sr', 'sv', 'tr',
+    'uk', 'zh-hans', 'zh-hant',
+))
+
+
+@method_decorator(login_required, name='dispatch')
+class CreateBillingView(FormView):
+    template_name = 'hosted/create.html'
+    form_class = ChooseBillingForm
+    success_url = 'https://weblate.org/{}/payment/?uuid={}'
+
+    def get_success_url(self, payment):
+        language = get_language()
+        if language not in SUPPORTED_LANGUAGES:
+            language = 'en'
+        return self.success_url.format('en', payment.uuid)
+
+    def form_valid(self, form):
+        payment = form.create_payment(self.request.user)
+        return HttpResponseRedirect(self.get_success_url(payment))
+
+    def get_context_data(self, **kwargs):
+        kwargs = super(CreateBillingView, self).get_context_data(**kwargs)
+        kwargs['plans'] = Plan.objects.filter(public=True).order_by('-price')
+        return kwargs
