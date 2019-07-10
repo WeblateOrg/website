bd637a3f017a7ccebe7e0f099758fb8cddf1144d
Michal Čihař <michal@cihar.com>
2019-07-10 13:35:28 +0200

    Use own filter for timestmap simplification
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ weblate_web/templatetags/timestamps.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2019 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+from django.template import Library
+from django.utils import timezone
+from django.utils.translation import pgettext
+
+register = Library()
+
+
+@register.filter
+def recently(value):
+    now = timezone.now()
+    delta = now - value
+    if delta.days > 12:
+        return pgettext("123 translations ...", "weeks ago")
+    if delta.days > 5:
+        return pgettext("123 translations ...", "week ago")
+    if delta.days >= 1:
+        return pgettext("123 translations ...", "days ago")
+    if delta.seconds > 10000:
+        return pgettext("123 translations ...", "hours ago")
+    if delta.seconds > 2000:
+        return pgettext("123 translations ...", "minutes ago")
+    return pgettext("123 translations ...", "just now")
