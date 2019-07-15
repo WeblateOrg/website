3a8a6bdc905e1f73a7ad5f1fbc76dd3ece2295fc
Michal Čihař <michal@cihar.com>
2019-07-15 17:11:33 +0200

    Initial code for donate page
    Currently only frontend without backend integration
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate_web/static/custom.js
+++ weblate_web/static/custom.js
@@ -13,7 +13,7 @@ $(function () {
 		}
 		return false;
 	});
-	
+
 	$('ul.pricing-tabs li').click(function(){
 		var tab_id = $(this).attr('data-tab');
 
@@ -22,8 +22,8 @@ $(function () {
 
 		$(this).addClass('current');
 		$("#"+tab_id).addClass('current');
-	})
-	
+	});
+
 	$('.pricing-table-tabs-menu ul li').click(function(){
 		var tab_id = $(this).attr('data-tab');
 
@@ -32,6 +32,45 @@ $(function () {
 
 		$(this).addClass('current');
 		$("#"+tab_id).addClass('current');
-	})
-	
+	});
+
+    $('.rewards .choose').click(function() {
+        var container = $($(this).parents('.reward')[0]);
+        container.parent().find('.reward').removeClass('checked');
+        container.addClass('checked');
+        container.find('input').prop('checked', true);
+        return false;
+    });
+    $('.rewards .close').click(function() {
+        $('.rewards .fourth .choose').click();
+        return false;
+    });
+    $('#donate-amount').change(function() {
+        var amount = $(this).val();
+        var found = 0;
+        $('.reward').each(function () {
+            var $this = $(this);
+            if ($this.data('amount') <= amount) {
+                if ($this.hasClass('small')) {
+                    $this.removeClass('small');
+                    $this.find('.choose').click();
+                }
+                found++;
+            } else {
+                $this.addClass('small');
+                if ($this.hasClass('checked')) {
+                    $this.find('.close').click();
+                }
+            }
+        });
+        console.log(found);
+        if (found > 1) {
+            $('.whoa').show();
+            $('.nowhoa').hide();
+        } else {
+            $('.whoa').hide();
+            $('.nowhoa').show();
+        }
+    });
+    $('#donate-amount').change();
 });
