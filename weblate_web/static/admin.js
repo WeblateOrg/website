c0110d0ac6432a0990ae542fde80a8626aeb5b88
Michal Čihař <michal@cihar.com>
2020-05-02 20:29:26 +0200

    Simplify code

--- weblate_web/static/admin.js
+++ weblate_web/static/admin.js
@@ -1,8 +1,5 @@
 window.addEventListener("load", function(){
-	var inputs = []
-	inputs = inputs.concat(document.querySelectorAll("input"));
-	inputs = inputs.concat(document.querySelectorAll("select"));
-	inputs = inputs.concat(document.querySelectorAll("textarea"));
+	var inputs = document.querySelectorAll("input,select,textarea");
 	if (inputs.length > 0){
 		for (var i=0, t=inputs.length; i<t ;i++){
 			if (inputs[i].parentNode.className.indexOf('submit-row') == -1){
