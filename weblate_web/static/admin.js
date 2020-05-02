fd218920f95d5a41fd0601715d2c82b98259739f
Michal Čihař <michal@cihar.com>
2020-05-02 20:26:11 +0200

    Activate code on all inputs

--- weblate_web/static/admin.js
+++ weblate_web/static/admin.js
@@ -1,8 +1,8 @@
 window.addEventListener("load", function(){
 	var inputs = []
-	inputs = inputs.concat(document.querySelector("input"));
-	inputs = inputs.concat(document.querySelector("select"));
-	inputs = inputs.concat(document.querySelector("textarea"));
+	inputs = inputs.concat(document.querySelectorAll("input"));
+	inputs = inputs.concat(document.querySelectorAll("select"));
+	inputs = inputs.concat(document.querySelectorAll("textarea"));
 	if (inputs.length > 0){
 		for (var i=0, t=inputs.length; i<t ;i++){
 			if (inputs[i].parentNode.className.indexOf('submit-row') == -1){
