fbdf1b952b791ef5afd9ba998fc688f5adc59a68
Michal Čihař <michal@cihar.com>
2020-06-30 10:32:45 +0200

    Lint: Apply prettier to own js/css code
    See https://github.com/WeblateOrg/weblate/issues/4110

--- weblate_web/static/admin.js
+++ weblate_web/static/admin.js
@@ -1,14 +1,18 @@
-window.addEventListener("load", function(){
-	var inputs = document.querySelectorAll("input,select,textarea");
-	if (inputs.length > 0){
-		for (var i=0, t=inputs.length; i<t ;i++){
-			if (inputs[i].parentNode.className.indexOf('submit-row') == -1){
-				inputs[i].addEventListener("change", function(){ window.onbeforeunload = function(){
-					return "Your changes have not been saved.";
-				}});
-			} else {
-				inputs[i].addEventListener("click", function(){ window.onbeforeunload = null });
-			}
-		}
-	}
-})
+window.addEventListener("load", function () {
+  var inputs = document.querySelectorAll("input,select,textarea");
+  if (inputs.length > 0) {
+    for (var i = 0, t = inputs.length; i < t; i++) {
+      if (inputs[i].parentNode.className.indexOf("submit-row") == -1) {
+        inputs[i].addEventListener("change", function () {
+          window.onbeforeunload = function () {
+            return "Your changes have not been saved.";
+          };
+        });
+      } else {
+        inputs[i].addEventListener("click", function () {
+          window.onbeforeunload = null;
+        });
+      }
+    }
+  }
+});
--- weblate_web/static/custom.js
+++ weblate_web/static/custom.js
@@ -1,115 +1,115 @@
 var _paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(['disableCookies']);
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-_paq.push(['setTrackerUrl', 'https://stats.cihar.com/matomo.php']);
-_paq.push(['setSiteId', '12']);
+_paq.push(["disableCookies"]);
+_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
+_paq.push(["setTrackerUrl", "https://stats.cihar.com/matomo.php"]);
+_paq.push(["setSiteId", "12"]);
 
 $(function () {
-	$('.menu-show').click(function(e) {
-		var $body = $('body');
-		$body.toggleClass('open-mobile');
-        $('.mobile-menu').toggle();
-	});
-	$('.open-langs').click(function(e) {
-		if($(this).parent().hasClass('opened')){
-			$(this).next().fadeOut(300);
-			$(this).parent().removeClass('opened');
-		} else {
-			$(this).next().fadeIn(300);
-			$(this).parent().addClass('opened');
-		}
-		return false;
-	});
+  $(".menu-show").click(function (e) {
+    var $body = $("body");
+    $body.toggleClass("open-mobile");
+    $(".mobile-menu").toggle();
+  });
+  $(".open-langs").click(function (e) {
+    if ($(this).parent().hasClass("opened")) {
+      $(this).next().fadeOut(300);
+      $(this).parent().removeClass("opened");
+    } else {
+      $(this).next().fadeIn(300);
+      $(this).parent().addClass("opened");
+    }
+    return false;
+  });
 
-	$('ul.pricing-tabs li').click(function(){
-		var tab_id = $(this).attr('data-tab');
+  $("ul.pricing-tabs li").click(function () {
+    var tab_id = $(this).attr("data-tab");
 
-		$('ul.pricing-tabs li').removeClass('current');
-		$('.tab-content').removeClass('current');
+    $("ul.pricing-tabs li").removeClass("current");
+    $(".tab-content").removeClass("current");
 
-		$(this).addClass('current');
-		$("#"+tab_id).addClass('current');
-	});
+    $(this).addClass("current");
+    $("#" + tab_id).addClass("current");
+  });
 
-	$('.pricing-table-tabs-menu ul li').click(function(){
-		var tab_id = $(this).attr('data-tab');
+  $(".pricing-table-tabs-menu ul li").click(function () {
+    var tab_id = $(this).attr("data-tab");
 
-		$('.pricing-table-tabs-menu ul li').removeClass('current');
-		$('.tab-content').removeClass('current');
+    $(".pricing-table-tabs-menu ul li").removeClass("current");
+    $(".tab-content").removeClass("current");
 
-		$(this).addClass('current');
-		$("#"+tab_id).addClass('current');
-	});
+    $(this).addClass("current");
+    $("#" + tab_id).addClass("current");
+  });
 
-    $('.rewards .choose').click(function() {
-        var container = $($(this).parents('.reward')[0]);
-        container.parent().find('.reward').removeClass('checked');
-        container.addClass('checked');
-        container.find('input').prop('checked', true);
-        return false;
-    });
-    $('.rewards .close').click(function() {
-        $('.rewards .fourth .choose').click();
-        return false;
-    });
-    $('#donate-amount').change(function() {
-        var amount = $(this).val();
-        var found = 0;
-        var highest = $('.reward.fourth');
-        $('.reward').each(function () {
-            var $this = $(this);
-            if ($this.data('amount') <= amount) {
-                if ($this.hasClass('small')) {
-                    $this.removeClass('small');
-                }
-                found++;
-                if (highest.data('amount') < $this.data('amount')) {
-                    highest = $this;
-                }
-            } else {
-                $this.addClass('small');
-                if ($this.hasClass('checked')) {
-                    $this.find('.close').click();
-                }
-            }
-        });
-        highest.find('.choose').click();
-        if (found > 1) {
-            $('.whoa').show();
-            $('.nowhoa').hide();
-        } else {
-            $('.whoa').hide();
-            $('.nowhoa').show();
+  $(".rewards .choose").click(function () {
+    var container = $($(this).parents(".reward")[0]);
+    container.parent().find(".reward").removeClass("checked");
+    container.addClass("checked");
+    container.find("input").prop("checked", true);
+    return false;
+  });
+  $(".rewards .close").click(function () {
+    $(".rewards .fourth .choose").click();
+    return false;
+  });
+  $("#donate-amount").change(function () {
+    var amount = $(this).val();
+    var found = 0;
+    var highest = $(".reward.fourth");
+    $(".reward").each(function () {
+      var $this = $(this);
+      if ($this.data("amount") <= amount) {
+        if ($this.hasClass("small")) {
+          $this.removeClass("small");
         }
-    });
-    $('#donate-amount').change();
-    $('#id_vat_0').on('change', function() {
-        var value = $(this).val();
-        if (value != '') {
-            var country = $('#id_country option[value="' + value + '"]');
-            country.prop('selected', true);
+        found++;
+        if (highest.data("amount") < $this.data("amount")) {
+          highest = $this;
         }
-    });
-    $('#id_vat_0,#id_vat_1').on('focusout', function() {
-        var country = $('#id_vat_0').val();
-        var code = $('#id_vat_1').val();
-        if (country && code) {
-            var payload = {
-                vat: country + code,
-                payment: $('input[name="payment"]').val(),
-                csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val(),
-            };
-            $.post('/js/vat/', payload , function(data) {
-                if (data.valid) {
-                    $('input[name="name"]').val(data.name);
-                    var parts = data.address.trim().split("\n");
-                    $('input[name="address"]').val(parts[0]);
-                    $('input[name="city"]').val(parts[parts.length - 1]);
-                }
-            });
+      } else {
+        $this.addClass("small");
+        if ($this.hasClass("checked")) {
+          $this.find(".close").click();
         }
+      }
     });
-    new ClipboardJS('[data-clipboard-text]');
+    highest.find(".choose").click();
+    if (found > 1) {
+      $(".whoa").show();
+      $(".nowhoa").hide();
+    } else {
+      $(".whoa").hide();
+      $(".nowhoa").show();
+    }
+  });
+  $("#donate-amount").change();
+  $("#id_vat_0").on("change", function () {
+    var value = $(this).val();
+    if (value != "") {
+      var country = $('#id_country option[value="' + value + '"]');
+      country.prop("selected", true);
+    }
+  });
+  $("#id_vat_0,#id_vat_1").on("focusout", function () {
+    var country = $("#id_vat_0").val();
+    var code = $("#id_vat_1").val();
+    if (country && code) {
+      var payload = {
+        vat: country + code,
+        payment: $('input[name="payment"]').val(),
+        csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val(),
+      };
+      $.post("/js/vat/", payload, function (data) {
+        if (data.valid) {
+          $('input[name="name"]').val(data.name);
+          var parts = data.address.trim().split("\n");
+          $('input[name="address"]').val(parts[0]);
+          $('input[name="city"]').val(parts[parts.length - 1]);
+        }
+      });
+    }
+  });
+  new ClipboardJS("[data-clipboard-text]");
 });
