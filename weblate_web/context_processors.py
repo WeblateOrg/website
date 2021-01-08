9d66c29515f24b54b1241a072cae0d73a61bae67
Michal Čihař <michal@cihar.com>
2021-01-08 10:34:51 +0100

    Fix context processor import

--- weblate_web/context_processors.py
+++ weblate_web/context_processors.py
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-import datetime
+from datetime import datetime
 from math import ceil
 
 from django.conf import settings
