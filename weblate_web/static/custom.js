26aa542e7986177f3502d1d42644b74a03b9961f
Creativetech <creative.joomdev@gmail.com>
2021-01-15 17:48:50 +0500

    Check on Custom JS file.

--- weblate_web/static/custom.js
+++ weblate_web/static/custom.js
@@ -208,9 +208,12 @@ ready(() => {
   new ClipboardJS("[data-clipboard-text]");
 });
 
+if (typeof SimpleSlider === 'function'){
+
 new SimpleSlider('.page-slider', {
   slidesPerView: {
     850: 2,
     1165: 3
   }
 });
+}
