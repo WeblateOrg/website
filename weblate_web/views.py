6d51fb91e5ffdda5ec18d78e986e1c4b043a97bc
Michal Čihař <michal@cihar.com>
2021-04-27 09:10:28 +0200

    News: Reduce number of pagination entries
    This is taken from Django admin and shows beginning, end and nearby pages.

--- weblate_web/views.py
+++ weblate_web/views.py
@@ -71,6 +71,42 @@ from weblate_web.models import (
 )
 from weblate_web.remote import get_activity
 
+ON_EACH_SIDE = 3
+ON_ENDS = 2
+DOT = "."
+
+
+def get_page_range(page_obj):
+    paginator = page_obj.paginator
+    page_num = page_obj.number
+
+    # If there are 10 or fewer pages, display links to every page.
+    # Otherwise, do some fancy
+    if paginator.num_pages <= 10:
+        page_range = range(paginator.num_pages)
+    else:
+        # Insert "smart" pagination links, so that there are always ON_ENDS
+        # links at either end of the list of pages, and there are always
+        # ON_EACH_SIDE links at either end of the "current page" link.
+        page_range = []
+        if page_num > (ON_EACH_SIDE + ON_ENDS):
+            page_range += [
+                *range(0, ON_ENDS),
+                DOT,
+                *range(page_num - ON_EACH_SIDE, page_num + 1),
+            ]
+        else:
+            page_range.extend(range(0, page_num + 1))
+        if page_num < (paginator.num_pages - ON_EACH_SIDE - ON_ENDS - 1):
+            page_range += [
+                *range(page_num + 1, page_num + ON_EACH_SIDE + 1),
+                DOT,
+                *range(paginator.num_pages - ON_ENDS, paginator.num_pages),
+            ]
+        else:
+            page_range.extend(range(page_num + 1, paginator.num_pages))
+    return page_range
+
 
 def get_customer(request):
     return Customer.objects.get_or_create(
@@ -557,6 +593,11 @@ class NewsArchiveView(ArchiveIndexView):
     date_field = "timestamp"
     paginate_by = 10
     ordering = ("-timestamp",)
+
+    def get_context_data(self, **kwargs):
+        result = super().get_context_data(**kwargs)
+        result["page_range"] = get_page_range(result["page_obj"])
+        return result
 
 
 class NewsView(NewsArchiveView):
