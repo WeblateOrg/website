9c308e5445eac8e66ea311e2bab0f969b0fda0a9
Michal Čihař <michal@cihar.com>
2022-09-15 13:01:35 +0200

    models: Make default for JSONField function instead of mutable

--- payments/models.py
+++ payments/models.py
@@ -202,9 +202,9 @@ class Payment(models.Model):
     )
     backend = models.CharField(max_length=100, default="", blank=True)
     # Payment details from the gateway
-    details = JSONField(default={}, blank=True)
+    details = JSONField(default=dict, blank=True)
     # Payment extra information from the origin
-    extra = JSONField(default={}, blank=True)
+    extra = JSONField(default=dict, blank=True)
     customer = models.ForeignKey(Customer, on_delete=models.deletion.CASCADE)
     repeat = models.ForeignKey(
         "Payment", on_delete=models.deletion.CASCADE, null=True, blank=True
