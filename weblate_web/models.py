040d32590ae37b5d5a116463544717f3272d6f91
Michal Čihař <michal@cihar.com>
2023-11-13 12:04:36 +0100

    api: increase timeout for Hetzner API
    Fixes WEBSITE-AV

--- weblate_web/models.py
+++ weblate_web/models.py
@@ -178,7 +178,7 @@ def create_backup_repository(service):
             "comment": f"Weblate backup service {service.pk}",
         },
         auth=(settings.STORAGE_USER, settings.STORAGE_PASSWORD),
-        timeout=10,
+        timeout=60,
     )
     data = response.json()
     return "ssh://{}@{}:23/./backups".format(
