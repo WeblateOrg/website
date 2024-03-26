82aff050d4d80ca6a0c912e7767d2081954586d1
Michal Čihař <michal@cihar.com>
2024-03-26 09:40:55 +0100

    payments: fix uuid save
    Based on https://github.com/django/django/commit/636d701ded62807612df50da37fea3adb88a4bb6

--- payments/models.py
+++ payments/models.py
@@ -76,6 +76,12 @@ class Char32UUIDField(models.UUIDField):
     def db_type(self, connection):
         return "char(32)"
 
+    def get_db_prep_value(self, value, connection, prepared=False):
+        value = super().get_db_prep_value(value, connection, prepared)
+        if value is not None:
+            value = value.hex
+        return value
+
 
 class Customer(models.Model):
     vat = VATINField(
