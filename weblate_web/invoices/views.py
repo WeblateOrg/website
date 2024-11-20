685711ef9501c2d9d4dbd515e9d9772ebd5a1757
Michal Čihař <michal@cihar.com>
2024-11-20 10:15:38 +0100

    feat: tell user that the invoice has been paid

--- weblate_web/invoices/views.py
+++ weblate_web/invoices/views.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.db import transaction
 from django.http import FileResponse, Http404
 from django.shortcuts import get_object_or_404, redirect
+from django.utils.translation import gettext
 
 from .models import Invoice, InvoiceKind
 
@@ -32,6 +34,15 @@ def pay_invoice(request: AuthenticatedHttpRequest, pk: str):
         Invoice, pk=pk, kind=InvoiceKind.INVOICE, paid_payment_set=None
     )
     if not invoice.can_be_paid():
+        if invoice.paid_payment_set.exists():
+            messages.info(
+                request,
+                gettext(
+                    "This invoice has already been paid. Please sign in to view details."
+                ),
+            )
+            return redirect("home")
+
         raise Http404("Cannot be paid")
     if invoice.draft_payment_set.exists():
         payment = invoice.draft_payment_set.all()[0]
