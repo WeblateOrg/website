58d1fc65d1f8e82f5ed846f3d90cc22e214f4a3d
Michal Čihař <michal@cihar.com>
2024-12-12 11:28:10 +0100

    fix: correct invoices ordering

--- weblate_web/invoices/management/commands/invoices_xml.py
+++ weblate_web/invoices/management/commands/invoices_xml.py
@@ -50,7 +50,7 @@ class Command(BaseCommand):
 
         invoices = Invoice.objects.filter(
             kind=InvoiceKind.INVOICE, issue_date__range=(date_start, date_end)
-        ).order_by("date_start")
+        ).order_by("issue_date")
 
         output_file = (
             settings.INVOICES_COPY_PATH
