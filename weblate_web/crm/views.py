7b9239fefff36696ce9d37d26f4be1dbad75c0ee
Michal Čihař <michal@cihar.com>
2025-05-27 13:27:51 +0200

    fix: default to blank search query

--- weblate_web/crm/views.py
+++ weblate_web/crm/views.py
@@ -142,7 +142,7 @@ class CustomerListView(CRMMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)  # type:ignore[misc]
-        context["query"] = self.request.GET.get("q")
+        context["query"] = self.request.GET.get("q", "")
         return context
 
     def get_queryset(self):
