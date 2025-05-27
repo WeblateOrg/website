88737cb5da3e2b1ad06c38a44c6e3d03684634e1
Michal Čihař <michal@cihar.com>
2025-05-27 11:53:54 +0200

    feat(crm): use dedicated storage

--- weblate_web/settings.py
+++ weblate_web/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+# CRM file storage
+CRM_ROOT = os.path.join(BASE_DIR, "crm")
+
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
