458f98a7002405f2053057319540f457c2454e62
Michal Čihař <michal@cihar.com>
2025-05-29 08:28:01 +0200

    feat: add core for zammad attachments sync

--- weblate_web/crm/models.py
+++ weblate_web/crm/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.files.storage import FileSystemStorage
 from django.db import models
+from django.utils import timezone
 
 from weblate_web.payments.models import Customer
 
@@ -12,8 +13,9 @@ class Interaction(models.Model):
     class Origin(models.IntegerChoices):
         EMAIL = 1, "Outboud e-mail"
         MERGE = 2, "Merged customer"
+        ZAMMAD_ATTACHMENT = 3, "Attachment exchanged in Zammad"
 
-    timestamp = models.DateTimeField(auto_now_add=True, verbose_name="Timestamp")
+    timestamp = models.DateTimeField(default=timezone.now, verbose_name="Timestamp")
     origin = models.IntegerField(choices=Origin, verbose_name="Origin")
     customer = models.ForeignKey(Customer, on_delete=models.RESTRICT)
     summary = models.CharField(max_length=200, verbose_name="Summary")
@@ -22,9 +24,23 @@ class Interaction(models.Model):
         storage=CRM_STORAGE, upload_to="attachments", verbose_name="Attachment"
     )
     user = models.ForeignKey(User, null=True, on_delete=models.RESTRICT)
+    remote_id = models.IntegerField(
+        verbose_name="Remote ID",
+        help_text="For example Zammad attachment ID",
+        default=0,
+    )
 
     class Meta:
         ordering = ["-timestamp"]
 
     def __str__(self):
         return f"{self.timestamp.isoformat()} [{self.customer}:{self.get_origin_display()}]: {self.summary}"
+
+
+class ZammadSyncLog(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True, verbose_name="Timestamp")
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    article_id = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return f"{self.customer}: {self.article_id}"
---
+++ weblate_web/management/commands/zammad_attachments.py
@@ -0,0 +1,64 @@
+#
+# Copyright © Michal Čihař <michal@weblate.org>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from __future__ import annotations
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from zammad_py import ZammadAPI
+
+from weblate_web.crm.models import ZammadSyncLog
+from weblate_web.payments.models import Customer
+
+
+class Command(BaseCommand):
+    help = "synchronizes attachments from Zammad"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def process_article(self, article_id: int, customer: Customer) -> None:
+        self.stdout.write(f"Processing article {article_id}")
+
+    def handle(self, *args, **options) -> None:
+        client = ZammadAPI(
+            url="https://care.weblate.org/api/v1/",
+            http_token=settings.ZAMMAD_TOKEN,
+        )
+        customers = Customer.objects.exclude(zammad_id=0)
+
+        processed_articles: set[int] = set(
+            ZammadSyncLog.objects.values_list("article_id", flat=True)
+        )
+
+        for customer in customers:
+            # Search for tickets with attachments from this customer
+            results = client.ticket.search(
+                f"article.attachment.title:* AND organization.id:{customer.zammad_id}"
+            )
+            while len(results):
+                # Process tickets and articles
+                for ticket in results:
+                    for article_id in ticket["article_ids"]:
+                        if article_id in processed_articles:
+                            continue
+                        processed_articles.add(article_id)
+                        # customer.zammadsynclog_set.create(article_id=article_id) # noqa: ERA001
+
+                results = results.next_page()
