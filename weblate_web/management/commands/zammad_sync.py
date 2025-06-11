6a8542adc6dc3b9812b2339d99da2604d11d15e2
Michal Čihař <michal@cihar.com>
2025-06-11 09:14:48 +0200

    fix: corrected premium support status

--- weblate_web/management/commands/zammad_sync.py
+++ weblate_web/management/commands/zammad_sync.py
@@ -99,7 +99,7 @@ class Command(BaseCommand):
             "last_payment": subscription.expires.date().isoformat(),
             "service_link": service.site_url,
             "premium_support": subscription.package.name == "premium"
-            and subscription.active,
+            and subscription.active(),
             "support": not subscription.is_expired,
             "plan": subscription.package.verbose,
         }
