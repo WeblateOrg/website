45bacfed31a1a2ffcee06f0cc68bf566147cd127
Michal Čihař <michal@cihar.com>
2025-11-18 15:21:26 +0100

    fix: avoid listing legacy plans

--- weblate_web/context_processors.py
+++ weblate_web/context_processors.py
@@ -73,7 +73,7 @@ def weblate_web(request):
         "hosting_packages": Package.objects.filter(
             category=PackageCategory.PACKAGE_SHARED
         )
-        .exclude(name__endswith="-m")
+        .filter(name__regex="^hosted:[0-9]+[km]$")
         .order_by("price")[:6],
         "language_columns": [
             language_urls[:language_col],
--- weblate_web/views.py
+++ weblate_web/views.py
@@ -1107,12 +1107,12 @@ class HostingView(TemplateView):
 
         data["hosted_package"] = (
             Package.objects.filter(category=PackageCategory.PACKAGE_SHARED)
-            .exclude(name__endswith="-m")
+            .filter(name__regex="^hosted:[0-9]+[km]$")
             .order_by("price")[0]
         )
         data["dedicated_package"] = (
             Package.objects.filter(category=PackageCategory.PACKAGE_DEDICATED)
-            .exclude(name__endswith="-m")
+            .filter(name__regex="^dedicated:[0-9]+[km]$")
             .order_by("price")[0]
         )
         data["basic_support_package"] = Package.objects.get(name="basic")
